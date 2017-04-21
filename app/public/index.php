99861d368d0e85debe769310102fe36d45945d89
Dollique <hello@codestyle.ch>
2017-04-21 09:20:41 +0200

    add DS instead of / to path to bootstrap.php

--- app/public/index.php
+++ app/public/index.php
@@ -1,5 +1,5 @@
 <?php
 	if(!defined('DS')) define('DS', DIRECTORY_SEPARATOR);
 	
-	require_once("../core/bootstrap.php");
+	require_once(realpath(__DIR__ .DS.'..') . DS ."core". DS . "bootstrap.php");
 ?>
